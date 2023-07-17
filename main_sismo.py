bad16f38b69b4a189c5b20d31ee548eab681644b
Aljiquita <98359502+Aljiquita@users.noreply.github.com>
2023-07-17 09:47:05 -0500

    2

--- main_sismo.py
+++ main_sismo.py
@@ -2,6 +2,8 @@ import streamlit as st
 from pydantic import BaseModel
 import joblib
 import numpy as np
+import os
+import base64
 
 #https://sismo-henry.streamlit.app/
 
@@ -67,6 +69,42 @@ def clasificar_sismo(Magnitud: float, Intensidad: float ):
 
 
 def main():
+    # Cargar la imagen de fondo
+    image_path = os.path.join(os.path.dirname(__file__), "imagen.avif")
+    with open(image_path, "rb") as file:
+        image_data = file.read()
+
+    # Convertir la imagen en base64
+    image_base64 = base64.b64encode(image_data).decode("utf-8")
+    
+    # Establecer el estilo de la página con CSS personalizado
+    page_bg_img = f'''
+        <style>
+        body {{
+            background-image: url(data:image/avif;base64,{image_base64});
+            background-size: cover;
+        }}
+        </style>
+    '''
+
+    st.title("Aplicación de multiplicación")
+
+    # Agregar el CSS personalizado a la página
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+    Magnitud = st.number_input("Ingrese La Magnitud", value=0.0)
+    Intensidad = st.number_input("Ingrese La Intensidad", value=0.0)
+
+    if st.button("clasificar"):
+        resultado = clasificar_sismo(Magnitud, Intensidad)
+        st.success(f"El resultado de la multiplicación es: {resultado}")
+
+"""
+if __name__ == '__main__':
+    main()
+
+
+def main():
     st.title("Aplicación de multiplicación")
     
     Magnitud = st.number_input("Ingrese La Magnitud", value=0.0)
@@ -79,3 +117,4 @@ def main():
 if __name__ == '__main__':
     main()
     
+"""
