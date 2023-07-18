adcbb03469ce80c2f9c0c2862e43dfcd7aa6444e
Aljiquita <98359502+Aljiquita@users.noreply.github.com>
2023-07-18 14:24:31 -0500

    1

--- main_sismo.py
+++ main_sismo.py
@@ -85,23 +85,12 @@ def main():
 
     st.title("Clasificación Sismo")
 
-   # Dividir la página en columnas
-    col1, col2 = st.columns(2)
+    Magnitud = st.number_input("Ingrese la magnitud:", value=0.0)
+    Intensidad = st.number_input("Ingrese la intensidad:", value=0.0)
 
-    # Columna izquierda: Formulario
-    with col1:
-        Magnitud = st.number_input("Ingrese la magnitud:", value=0.0)
-        Intensidad = st.number_input("Ingrese la intensidad:", value=0.0)
-        if st.button("Clasificar formulario"):
-            resultado = clasificar_sismo(Intensidad, Magnitud)
-
-    # Columna derecha: Resultado
-    with col2:
-        if st.button("Clasificar resultado"):
-            st.success(resultado["texto"])
-
-
-
+    if st.button("Clasificar"):
+        resultado = clasificar_sismo(Intensidad, Magnitud )
+        st.success(resultado["texto"])
 
     # Agregar los textos debajo del formulario
     st.write("")
